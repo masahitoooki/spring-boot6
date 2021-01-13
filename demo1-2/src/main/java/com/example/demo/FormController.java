042a48b0bf0224d75a204134efd7fb53c153ff42
masahitoooki <58727760+masahitoooki@users.noreply.github.com>
2021-01-13 11:23:17 +0900

    Add files via upload

---
+++ demo1-2/src/main/java/com/example/demo/FormController.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/form")
+public class FormController {
+
+    @GetMapping
+    public String init(Model model) {
+        model.addAttribute(new MyForm());
+        return "form";
+    }
+
+    @PostMapping(params="appendRow")
+    public String appendRow(MyForm form) {
+        form.appendRow();
+        this.printRows(form);
+        return "form";
+    }
+
+    @PostMapping(params="removeIndex")
+    public String submit(MyForm form, @RequestParam int removeIndex) {
+        form.removeRow(removeIndex);
+        this.printRows(form);
+        return "form";
+    }
+
+    private void printRows(MyForm form) {
+        List<MyForm.Row> rows = form.getRows();
+        for (int i = 0; i < rows.size(); i++) {
+            MyForm.Row row = rows.get(i);
+            System.out.println("i=" + i + ", row.value=" + row.getValue());
+        }
+    }
+}
---
+++ demo1-2/src/main/java/com/example/demo/MyForm.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyForm {
+    private List<Row> rows = new ArrayList<>();
+
+    public void appendRow() {
+        this.rows.add(new Row());
+    }
+
+    public void removeRow(int index) {
+        this.rows.remove(index);
+    }
+
+    public List<Row> getRows() {
+        return rows;
+    }
+
+    public void setRows(List<Row> rows) {
+        this.rows = rows;
+    }
+
+    public static class Row {
+        private String value;
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+    }
+}
